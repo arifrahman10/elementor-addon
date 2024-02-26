399a13675c998920f33f90b8504c27ed91d4b9c4
Arif Rahman <arifbb79@gmail.com>
2024-02-26 20:19:01 +0600

    Update: Heading control tabs

--- widgets/Heading.php
+++ widgets/Heading.php
@@ -60,7 +60,7 @@ class Elementor_Addons_Heading extends \Elementor\Widget_Base {
         $this->start_controls_section(
             'style_section',
             [
-                'label' => esc_html__( 'Title Style', 'textdomain' ),
+                'label' => esc_html__( 'Title', 'textdomain' ),
                 'tab' => \Elementor\Controls_Manager::TAB_STYLE,
             ]
         );
@@ -83,7 +83,147 @@ class Elementor_Addons_Heading extends \Elementor\Widget_Base {
             ]
         );
 
+
+        //====== Tabs
+        $this->start_controls_tabs(
+            'style_title_tabs'
+        );
+
+        // Tab Normal
+        $this->start_controls_tab(
+            'style_normal_tab', [
+                'label' => esc_html__( 'Normal', 'textdomain' ),
+            ]
+        );
+
+        $this->add_control(
+            'normal_text_color', [
+                'label' => esc_html__( 'Text Color', 'textdomain' ),
+                'type' => \Elementor\Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} .title a' => 'color: {{VALUE}}',
+                ],
+            ]
+        );
+
+
+        $this->add_control(
+            'normal_bg_color', [
+                'label' => esc_html__( 'Background Color', 'textdomain' ),
+                'type' => \Elementor\Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} .title a' => 'background-color: {{VALUE}}',
+                ],
+            ]
+        );
+
+        $this->add_group_control(
+            \Elementor\Group_Control_Border::get_type(),
+            [
+                'name' => 'border',
+                'selector' => '{{WRAPPER}} .title a',
+            ]
+        );
+
+        $this->add_group_control(
+            \Elementor\Group_Control_Box_Shadow::get_type(),
+            [
+                'name' => 'normal_box_shadow',
+                'selector' => '{{WRAPPER}} .title a',
+            ]
+        );
+
+        $this->end_controls_tab();//Tab Normal
+
+        // Tab Hover
+        $this->start_controls_tab(
+            'style_hover_tab', [
+                'label' => esc_html__( 'Hover', 'textdomain' ),
+            ]
+        );
+
+        $this->add_control(
+            'hover_text_color', [
+                'label' => esc_html__( 'Text Color', 'textdomain' ),
+                'type' => \Elementor\Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} .title a:hover' => 'color: {{VALUE}}',
+                ],
+            ]
+        );
+
+        $this->end_controls_tab();//Tab Hover
+
+
+        // Tab Active
+        $this->start_controls_tab(
+            'style_active_tab', [
+                'label' => esc_html__( 'Active', 'textdomain' ),
+            ]
+        );
+
+        $this->end_controls_tab();//End Tab Active
+
+
+        $this->end_controls_tabs();//End Tabs
+
+
+        //=== Padding
+        $this->add_responsive_control(
+            'padding', [
+                'label' => esc_html__( 'Padding', 'textdomain' ),
+                'type' => \Elementor\Controls_Manager::DIMENSIONS,
+                'size_units' => [ 'px', '%', 'em', 'rem', 'custom' ],
+                'default' => [
+                    'top' => 2,
+                    'right' => 0,
+                    'bottom' => 2,
+                    'left' => 0,
+                    'unit' => 'em',
+                    'isLinked' => false,
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .title a' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                ],
+                'separator' => 'before'
+            ]
+        );
+
+
         $this->end_controls_section(); // End Title Style
+
+
+        //=================== Start Test Style ===============//
+        $this->start_controls_section(
+            'style_section_2',
+            [
+                'label' => esc_html__( 'Another Section', 'textdomain' ),
+                'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+            ]
+        );
+
+        //=== Padding
+        $this->add_responsive_control(
+            'padding_2', [
+                'label' => esc_html__( 'Padding', 'textdomain' ),
+                'type' => \Elementor\Controls_Manager::DIMENSIONS,
+                'size_units' => [ 'px', '%', 'em', 'rem', 'custom' ],
+                'default' => [
+                    'top' => 2,
+                    'right' => 0,
+                    'bottom' => 2,
+                    'left' => 0,
+                    'unit' => 'em',
+                    'isLinked' => false,
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .title a' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                ],
+                'separator' => 'before'
+            ]
+        );
+
+        $this->end_controls_section(); //Test Section
 
 
     }
